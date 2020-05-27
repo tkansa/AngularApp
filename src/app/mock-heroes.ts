b28b704ab54c19e63a1d3d83c9ebae99f598ddb5
Tiia Kansa <como2e2@gmail.com>
2020-05-27 11:27:07 -0400

    First commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './interfaces/hero'
+
+export const HEROES : Hero[] = [
+    
+    {id: 0, name: "Marvin"},
+    {id: 1, name: "Moe"},
+    {id: 2, name: "Yuri"},
+    {id: 3, name: "Tiia"},
+    {id: 4, name: "Breck"},
+    {id: 5, name: "Jake"},
+    {id: 6, name: "Nova"},
+
+]
